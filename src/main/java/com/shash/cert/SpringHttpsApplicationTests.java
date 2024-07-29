de762f4a84539424737049d72421c9d2861f7655
root <root@appsecserver.us-east4-a.c.gcp-clqa-security-tvm.internal>
2024-07-29 06:43:46 +0000

    Changes for Migration of Java 17

---
+++ src/main/java/com/shash/cert/SpringHttpsApplicationTests.java
@@ -0,0 +1,14 @@
+package com.shash.cert;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+
+class SpringHttpsApplicationTests {
+
+
+	void contextLoads() {
+	}
+
+}
